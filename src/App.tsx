eebc534c5db1f1a6fa4349a39424f9cc76f13483
roshanyadav-2109 <businessmarketing0002@gmail.com>
2025-07-01 14:31:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import AppRoutes from "./AppRoutes";
 import NotificationToast from "./components/NotificationToast";
+import { AuthProvider } from "./hooks/useAuth"; // Add this import
 
 const queryClient = new QueryClient();
 
@@ -11,9 +11,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <AppRoutes />
-        <NotificationToast />
-        <Toaster />
+        <AuthProvider> {/* Add AuthProvider inside BrowserRouter */}
+          <AppRoutes />
+          <NotificationToast />
+          <Toaster />
+        </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
   );
